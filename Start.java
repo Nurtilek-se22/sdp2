39eceda11e8d7c505ee44e40685ec58517e0bf77
Nurtilek-se22 <sergeinurtilek@gmail.com>
2025-09-23 23:18:50 +0500

    Start.java

--- Start.java
+++ Start.java
@@ -2,18 +2,33 @@ import factory.impl.RussianFactory;
 import factory.impl.USAFactory;
 import factory.interfaces.TransportFactory;
 import transport.interfaces.Aircraft;
+import transport.interfaces.Car;
 
 public class Start {
     private static TransportFactory factory;
-    public static void main(String[] args) {
-        if (true){
-            factory = new RussianFactory();
-        }
-        else{
-            factory = new USAFactory();
-        }
-        factory.createAircraft();
-        factory.createCar();
 
+    public static void main(String[] args) {
+        boolean useRussian = true;
+        if (useRussian) {
+            factory = new RussianFactory();
+            System.out.println("== Russian Factory Selected ==");
+        } else {
+            factory = new USAFactory();
+            System.out.println("== USA Factory Selected ==");
+        }
+
+        Car car = factory.createCar();
+        Aircraft aircraft = factory.createAircraft();
+
+        System.out.println("-- Car Actions --");
+        car.drive();
+        car.stop();
+
+        System.out.println("-- Aircraft Actions --");
+        try {
+            aircraft.flight();
+        } catch (UnsupportedOperationException e) {
+            System.out.println("Aircraft flight is not implemented yet.");
+        }
     }
 }
